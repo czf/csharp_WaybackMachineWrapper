afccad4c60c7d41331af85008b59fca61017f01b
Chris F <fingarc@gmail.com>
2017-04-14 17:52:58 -0700

    Initial commit
    created wrapper and tests for WaybackMachine API.

---
+++ NUnit.Tests/TestClass.cs
@@ -0,0 +1,122 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WaybackMachineWrapper;
+using RestSharp;
+using Moq;
+using System.Net;
+using System.Net.Http;
+using RestSharp.Extensions;
+using System.Globalization;
+using System.Threading;
+using System.IO;
+
+namespace NUnit.Tests
+{
+    [TestFixture]
+    public class WaybackCLient_Tests
+    {
+        private const string RESPONSE_AVAILABLE = @"{
+    ""archived_snapshots"": {
+                        ""closest"": {
+                            ""available"": true,
+			""url"": ""http://web.archive.org/web/20170312090534/http://www.seattletimes.com/seattle-news/crime/help-me-kill-my-wife-man-accidentally-texts-to-his-former-boss/"",
+			""timestamp"": ""20170312090534"",
+			""status"": ""200""   
+        }
+                    }
+                }";
+
+        [Test]
+        public void TestMockGet()
+        {
+
+            HttpMessageHandler mockMessageHandler = new MockMessageHandler(new MockHttpContent(RESPONSE_AVAILABLE));
+            WaybackClient client = new WaybackClient(new HttpClient(mockMessageHandler));
+
+            string original = "http://www.something.com";
+            AvailableResponse ar = client.Available(new Uri(original));
+            
+            Assert.IsNotNull(ar);
+            Assert.IsNotNull(ar.archived_snapshots);
+            Assert.IsNotNull(ar.archived_snapshots.closest);
+            Assert.IsNotNull(ar.RequestUrl);
+            Assert.IsTrue(ar.archived_snapshots.closest.available);
+            Assert.AreEqual(ar.RequestUrl.OriginalString, original);
+            Assert.AreEqual(ar.archived_snapshots.closest.status, HttpStatusCode.OK);
+        }
+
+        [Test]
+        public void TestSeattleTimes()
+        {
+            WaybackClient client = new WaybackClient();
+            AvailableResponse response = client.Available(new Uri("http://www.seattletimes.com"));
+        }
+
+
+        [Test]
+        public void TestMockSave()
+        {
+            MockHttpContent content = new MockHttpContent(string.Empty);
+            content.Headers.ContentLocation = new Uri("http://www.expectedoutput.com");
+            HttpMessageHandler mockMessageHandler = new MockMessageHandler(content);
+            WaybackClient client = new WaybackClient(new HttpClient(mockMessageHandler));
+
+
+            Uri location = client.Save(new Uri("http://www.uritosave.com"));
+
+            Assert.IsNotNull(location);
+            Assert.AreEqual(content.Headers.ContentLocation.AbsoluteUri, location.AbsoluteUri);
+        }
+
+        //[Test]
+        //public void Save()
+        //{
+        //    WaybackClient client = new WaybackClient();
+        //    Uri location = client.Save(new Uri("http://www.zappos.com/p/nike-shox-nz-wolf-grey-metallic-gold-anthracite/product/7395033/color/673110"));
+        //    Assert.IsNotNull(location);
+        //    Assert.IsTrue(location.AbsoluteUri.Contains("archive.org") && location.AbsoluteUri.Contains("zappos.com"));
+        //}
+    }
+
+    internal class MockMessageHandler : HttpMessageHandler
+    {
+        public HttpContent MockResponseContent;
+        public MockMessageHandler(HttpContent content)
+        {
+            MockResponseContent = content;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            HttpResponseMessage result = new HttpResponseMessage();
+            result.Content = MockResponseContent;
+            return Task.FromResult<HttpResponseMessage>(result);
+        }
+    }
+
+    internal class MockHttpContent : HttpContent
+    {
+        public string Content { get; set; }
+
+        public MockHttpContent(string content)
+        {
+            Content = content;
+        }
+
+        protected override Task SerializeToStreamAsync(Stream stream, TransportContext context)
+        {
+            byte[] byteArray = Encoding.ASCII.GetBytes(Content);
+            return stream.WriteAsync(byteArray, 0, byteArray.Length);
+        }
+
+        protected override bool TryComputeLength(out long length)
+        {
+            length = Content.Length;
+            return true;
+        }
+    }
+}
---
+++ csharp_WaybackMachineWrapper/WaybackClient.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("NUnit.Tests")]
+namespace WaybackMachineWrapper
+{
+    public class WaybackClient : IDisposable
+    {
+        #region static/consts
+        private static readonly Uri BASE_URI = new Uri("http://web.archive.org/");
+        private const string AVAILABLE_PATH = "wayback/available/?url=";
+        private const string SAVE_PATH = "save/";
+        #endregion
+
+        #region private
+        private HttpClient _client;
+        //private IRestRequestFactory _restRequestFactory;
+        #endregion
+
+        #region Constructors
+        public WaybackClient() : this(new HttpClient()) { }
+
+        internal WaybackClient(HttpClient client)
+        //public WaybackClient( restClient, IRestRequestFactory restRequestFactory)
+        {
+            _client = client;
+            _client.BaseAddress = BASE_URI;
+        }
+                
+        #endregion
+        #region public
+        public async Task<AvailableResponse> AvailableAsync(Uri uri )
+        {
+            //IRestRequest request = _restRequestFactory.Create(AVAILABLE_PATH, Method.GET);
+            //request.AddParameter(AVAILABLE_PARAMETER, uri);
+            AvailableResponse result = null;
+            
+            //IRestResponse response = await _restClient.ExecuteGetTaskAsync(request);
+            using (HttpResponseMessage response = await _client.GetAsync(AVAILABLE_PATH + uri))
+            {
+                using (HttpContent content = response.Content)
+                {
+                    string json = await content.ReadAsStringAsync();
+                    result = JsonConvert.DeserializeObject<AvailableResponse>(json);
+                    if(result != null) { result.RequestUrl = uri; }
+                }
+            }
+                
+            
+            return result;
+        }
+
+        public AvailableResponse Available(Uri uri)
+        {
+            Task<AvailableResponse> responseTask = AvailableAsync(uri);
+            Task.WaitAll(responseTask);
+            return responseTask.Result;
+        }
+
+        public async Task<Uri> SaveAsync(Uri uri)
+        {
+            Uri result = null;
+            using (HttpResponseMessage response = await _client.GetAsync(SAVE_PATH + uri))
+            {
+                if (response.StatusCode == HttpStatusCode.OK || response.StatusCode == HttpStatusCode.Redirect)
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        
+                        result = content.Headers.ContentLocation;
+                        if (!result.IsAbsoluteUri)
+                        {
+                            result = new Uri(BASE_URI, result.OriginalString);
+                        }
+
+                    }
+                }
+            }
+            return result;
+        }
+
+        public Uri Save(Uri uri)
+        {
+            Task<Uri> responseTask = SaveAsync(uri);
+            Task.WaitAll(responseTask);
+            return responseTask.Result;
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+        #endregion
+    }
+    
+    public class AvailableResponse
+    {
+        public ArchivedSnapshots archived_snapshots {get; set;}
+        public Uri RequestUrl { get; set; }
+
+        public AvailableResponse() { }
+        public AvailableResponse(Uri requestUrl)
+        {
+            RequestUrl = requestUrl;
+        }
+
+        public class ArchivedSnapshots
+        {
+            public ArchivedSnapshot closest { get;  set; }
+            public class ArchivedSnapshot
+            {
+                public bool available { get; set; }
+                public Uri url { get; set; }
+                public string timestamp { get; set; }
+                public System.Net.HttpStatusCode status { get; set; }
+            }
+            
+        }
+       
+    }
+}
